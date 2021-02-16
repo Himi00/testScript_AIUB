ab3a3c3569d20ca1fb89bebd6b1f16983a811e5a
himi00 <himihumayra97@gmail.com>
2021-02-16 21:40:09 +0600

    updated

--- src/aiub/test.java
+++ src/aiub/test.java
@@ -12,11 +12,41 @@ public class test {
         WebDriver driver = new ChromeDriver();
         driver.get("https://portal.aiub.edu/");
         
-          driver.findElement(By. name("UserName")).sendKeys("himi");
+          driver.findElement(By. name("UserName")).sendKeys("17-34572-2");
           driver.findElement(By.name("Password")).sendKeys("Humayra1*");
           driver.findElement(By.xpath("/html/body/div/div/div/div/div[2]/form/div[4]/button")).click();
         
-        String exp_title= "Index";
+        
+        driver.findElement(By.xpath("//*[@id='main-content']/div/div/div[3]/div[2]/div/div[1]/div[2]/div/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notes']/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notices']/a")).click();
+        driver.findElement(By.xpath("//*[@id='navigation-toggler']")).click();
+        
+        driver.findElement(By.xpath("//*[@id='main-content']/div/div/div[3]/div[2]/div/div[2]/div[2]/div[1]/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notes']/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notices']/a")).click();
+        driver.findElement(By.xpath("//*[@id='navigation-toggler']")).click();
+
+        driver.findElement(By.xpath("//*[@id='main-content']/div/div/div[3]/div[2]/div/div[2]/div[2]/div[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notes']/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notices']/a")).click();
+        driver.findElement(By.xpath("//*[@id='navigation-toggler']")).click();
+
+        driver.findElement(By.xpath("//*[@id='main-content']/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notes']/a")).click();
+        driver.findElement(By.xpath("//*[@id='Notices']/a")).click();
+        driver.findElement(By.xpath("//*[@id='navigation-toggler']")).click();
+        
+
+   driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-00000000-0000-0000-0000-000000000000']/ul[2]/li/p/a/small")).click();
+//        driver.findElement(By.xpath("//*[@id='Notices']/a")).click();
+//        driver.findElement(By.xpath("//*[@id='Notices']/a")).click();
+//        driver.findElement(By.xpath("//*[@id='Notices']/a")).click();
+
+//        driver.findElement(By.xpath("//*[@id='collapse25da6797-419c-41cd-82d5-7c6ac0e9602b']/div/a[1]")).click();
+
+        
+        String exp_title= "Profile";
         String act_title = driver.getTitle();
         
         if (exp_title.equals(act_title) == true)
@@ -25,6 +55,7 @@ public class test {
         }
         else {
       	  System.out.println("Failed");
+      	  
 
         }
 
